77be0dfb1ffe95445e390ab5b8ba7e68193ca3d6
rohinisharma <rasberry314@gmail.com>
2020-04-25 22:08:08 -0400

    Revert "Merge branch"
    This reverts commit e61d2af39f189b14f152a2431bf52a4f12f3c7cf.

--- src/App.js
+++ src/App.js
@@ -1,157 +1,29 @@
-import React, { Component } from "react";
-import DayContainer from "./containers/DayContainer";
-import Navbar from "./components/Navbar";
-import AuthFormContainer from "./containers/AuthFormContainer";
-import { Route, Switch, withRouter } from "react-router-dom";
-import api from "./services/api";
+import React, { useState, useEffect } from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-class App extends Component {
-  state = {
-    auth: {
-      currentUser: {
-        appointments: [],
-        startTime: "0000",
-        endTime: "2300"
-      }
-    },
-    reminder: this.props.reminder,
-    modalOpen: false
-  };
-  onEscape = e => {
-    if (e.key === "Escape") {
-      this.modalClose();
-    }
-  };
-  modalClose = () => {
-    this.setState({ modalOpen: false });
-  };
-componentDidMount() {
-    const token = localStorage.getItem("token");
-    if (token) {
-      api.auth.getCurrentUser().then(user => {
-        const currentUser = { currentUser: user };
-        this.setState({
-          auth: currentUser
-        });
-      });
-    } else {
-      this.props.history.push("/login");
-    }
-  }
-  handleLogin = user => {
-    const currentUser = { currentUser: user };
-    localStorage.setItem("token", user.token);
-    this.setState({
-      auth: currentUser
-    });
-  };
-
-  handleLogout = () => {
-    localStorage.removeItem("token");
-    this.setState({ auth: { currentUser: {} } });
-  };
-
-  createAppointment = appointment => {
-    const prevState = this.state.auth.currentUser.appointments.slice();
-    prevState.push(appointment);
-    this.setState({
-      auth: {
-        currentUser: { ...this.state.auth.currentUser, appointments: prevState }
-      }
-    });
-    if (prevState.length === 1) {
-      this.setState({ modalOpen: true });
-    }
-  };
-
-  handleDelete = id => {
-    api.appointments.deleteAppointment(id);
-    const prevState = this.state.auth.currentUser.appointments.slice();
-    const newState = prevState.filter(a => a.id.toString() !== id);
-    this.setState({
-      auth: {
-        currentUser: { ...this.state.auth.currentUser, appointments: newState }
-      }
-    });
-  };
-  setTimes = time => {
-    api.users.update(
-      this.state.auth.currentUser.id,
-      time.startTime,
-      time.endTime
-    );
-    this.setState({
-      auth: {
-        currentUser: {
-          ...this.state.auth.currentUser,
-          startTime: time.startTime,
-          endTime: time.endTime
-        }
-      }
-    });
-  };
-
-  render() {
-    return (
-      <div onKeyUp={this.onEscape}>
-        <div>
-          <Navbar
-            currentUser={this.state.auth.currentUser}
-            handleLogout={this.handleLogout}
-          />
-        </div>
-        <Switch>
-          <Route
-            path="/"
-            render={routerProps => {
-              return (
-                <DayContainer
-                  style={{ height: "100vh" }}
-                  handleChange={this.handleChange}
-                  setTimes={this.setTimes}
-                  createAppointment={this.createAppointment}
-                  currentUser={this.state.auth.currentUser}
-                  handleDelete={this.handleDelete}
-                  quote={this.state. reminder}
-                  modalOpen={this.state.modalOpen}
-                  handleClose={this.handleClose}
-                  modalClose={this.modalClose}
-                />
-              );
-            }}
-          />
-        </Switch>
-        <div
-          style={{
-            position: "absolute",
-            bottom: "10px",
-            right: "10px"
-          }}
+function App() {
+  
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
         >
-          <footer>
-            <p
-              style={{
-                color: "RGB(31,31,31)",
-                backgroundColor: "RGBa(244,244,244, 0)",
-                borderTopRightRadius: "20px",
-                borderTopLeftRadius: "5px",
-                borderBottomRightRadius: "5px",
-                borderBottomLeftRadius: "5px"
-              }}
-            >
-            {" "}
-              <a
-                style={{ color: "RGB(14,14,14)" }}
-                href=" "
-              >
-                
-              </a>
-            </p>
-          </footer>
-        </div>
-      </div>
-    );
-  }
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
-export default withRouter(App);
+
+
+export default App;
--- src/components/Time.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Time = props => {
-  return <td className="left aligned">{props.time}</td>;
-};
-
-export default Time;
